200ab6c06d0820a80f1c24b57d87172ac150c95e
Katie Blizzard <kblizzard11@gmail.com>
2018-12-15 21:18:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 
-document.querySelector(main)
+document.querySelector('main').remove()
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
