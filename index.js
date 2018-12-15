892cf9a1b5cea152c84098c6404da9bc7a259e42
Katie Blizzard <kblizzard11@gmail.com>
2018-12-15 21:27:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
 
-document.querySelector('main').remove()
+document.querySelector('main').remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
-var victory = document.createElement('h1')
-victory.id = "victory"
-victory.innerHTML = "Katie is the champion!"
+var newHeader = document.createElement('h1#victory');
+
+newHeader.innerHTML = "Katie is the champion!";
