8ad7f63cb2d5ad0748d491c1a5ae590584f4fe8e
Katie Blizzard <kblizzard11@gmail.com>
2018-12-15 21:23:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ document.querySelector('main').remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 var victory = document.createElement('h1')
+victory.innerHTML('id = "victory"')
